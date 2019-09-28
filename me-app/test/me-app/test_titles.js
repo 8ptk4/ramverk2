9b2371cccbd7d4ff6754a6f84d1efbaee8572b4e
8ptk4 <putte.karlsson@gmail.com>
2019-09-28 20:35:01 +0200

    added selenium, mocha, http-sever

---
+++ me-app/test/me-app/test_titles.js
@@ -0,0 +1,33 @@
+const assert = require("assert");
+const test = require("selenium-webdriver/testing");
+const webdriver = require("selenium-webdriver");
+const By = require("selenium-webdriver").By;
+
+let browser;
+
+// Test Suite
+test.describe("React App", function() {
+    test.beforeEach(function(done) {
+        this.timeout(20000);
+        browser = new webdriver.Builder().
+            withCapabilities(webdriver.Capabilities.firefox()).build();
+
+        browser.get("http://localhost:3000/");
+        done();
+    });
+
+    test.afterEach(function(done) {
+        browser.quit();
+        done();
+    });
+
+    // Test Case
+    test.it("Test Index", function(done) {
+        browser.getTitle().then(function(title) {
+            assert.equal(title, "React App");
+        });
+
+        done();
+    });
+});
+
